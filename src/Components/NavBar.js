59434dbb18292b738376cd932cdde59b1c089816
Godfri John <godfrijunior24@gmail.com>
2024-09-09 21:08:19 +0530

    merch
    change kra

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -25,7 +25,6 @@ const NavBar = () => {
     <a href="/"><img src={logo} alt="" className ="logo"/></a>
       <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li><RLink to="/" smooth={true} offset={-200} duration={500}>Home</RLink></li>
-        <li><Link to="AbNlds" smooth={true} offset={-200} duration={500}>About NLDS</Link></li>
         <li><RLink to="Gallery" smooth={true} offset={-250} duration={500}>Gallery</RLink></li>
         <li><RLink to="/OC" smooth={true} offset={-250} duration={500}>OC</RLink></li>
         <li><RLink to="/Faci" smooth={true} offset={-250} duration={500}>Faci</RLink></li>
--- src/Pages/MerchHome.js
+++ src/Pages/MerchHome.js
@@ -1,12 +1,14 @@
 import React from "react";
 import MerchHero from "../Components/MerchHero";
 import NavBar from "../Components/NavBar";
+import MerchSlider from "../Components/MerchSlider";
 
 const MerchHome = () => {
   return (
     <div id="MerchHome">
       <NavBar />
       <MerchHero />
+      <MerchSlider/>
     </div>
   );
 };
