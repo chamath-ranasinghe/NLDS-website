b69f80cce3769328bf2ca9e25217e81abb0ed3d3
ChamathJ <ranasingherpcgj.21@uom.lk>
2024-09-16 19:41:46 +0530

    Prices updated in page

--- src/Components/ClickInfoSection.js
+++ src/Components/ClickInfoSection.js
@@ -4,6 +4,10 @@ function ClickInfoSection() {
   return (
     <section className="click-info-section">
       <p className="click-info-text">Click on the items to view details</p>
+      <p></p>
+      <p className="click-info-text prices">Standard Pack: Tshirt + Band + Sticker Pack + Mystery Gift (LKR 1850)</p>
+      <p className="click-info-text prices">Skinny (LKR 950)</p>
+      <p className="click-info-text prices">Cap (LKR 750)</p>
     </section>
   )
 }
