17268f0b9d3153e3eeb5f5d83241802321a22ca2
ChamathJ <ranasingherpcgj.21@uom.lk>
2024-09-26 01:21:04 +0530

    Merch Orders Closed 2

--- src/Components/ClickInfoSection.js
+++ src/Components/ClickInfoSection.js
@@ -8,10 +8,10 @@ function ClickInfoSection() {
       <p className="click-info-text prices">Standard Pack: Tshirt + Band + Sticker Pack + Mystery Gift (LKR 1850)</p>
       <p className="click-info-text prices">Skinny (LKR 950)<span style={{color:"#f00"}}>  Limited Edition </span> </p>
       <p className="click-info-text prices">Cap (LKR 750)<span style={{color:"#f00"}}>  Limited Edition </span></p>
-      <p className="click-info-text prices">Cap and Skinny (LKR 1600)<span style={{color:"#f00"}}> Till 24th September (11.59 PM) only </span></p>
+      {/* <p className="click-info-text prices">Cap and Skinny (LKR 1600)<span style={{color:"#f00"}}> Till 24th September (11.59 PM) only </span></p> */}
       <p className="click-info-text prices"></p>
       <p className="click-info-text prices"></p>
-      <p className="click-info-text prices"> <span style={{fontSize:"120%"}}>Hurry! Only open till 24th September - 11.59 PM </span></p>
+      {/* <p className="click-info-text prices"> <span style={{fontSize:"120%"}}>Hurry! Only open till 24th September - 11.59 PM </span></p> */}
     </section>
   )
 }
