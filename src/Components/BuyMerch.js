703304aafc000474bbf17715fbed78e56df6a93c
ChamathJ <ranasingherpcgj.21@uom.lk>
2024-11-01 09:45:48 +0530

    Merch Opened

--- src/Components/BuyMerch.js
+++ src/Components/BuyMerch.js
@@ -9,13 +9,13 @@ function BuyMerch() {
                     <img src={merch} alt="Logo" className="merch-items" />
                 </div>
                 <div className="buy-text">
-        {/* <p>CLICK HERE TO BUY THE MERCH PACK</p> */}
-        {/* <a href="https://forms.gle/DZbGMCKoFzqBwt92A" target="_blank" rel="noopener noreferrer" className="buy-text-link">
+        <p>CLICK HERE TO BUY THE MERCH PACK</p>
+        <a href="https://forms.gle/DZbGMCKoFzqBwt92A" target="_blank" rel="noopener noreferrer" className="buy-text-link">
           CLICK HERE
-        </a> */}
-        <a href="#" className="buy-text-link">
-          Pre Orders are closed :(
         </a>
+        {/* <a href="#" className="buy-text-link">
+          Pre Orders are closed :(
+        </a> */}
       </div>
             </div>
     </div>
