1947874d94aff768b8ddffe64596351e8e216f7a
chamath-ranasinghe <chamathmmw@gmail.com>
2025-02-25 07:39:03 +0000

    Started on a Error page

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
-  output: 'standalone', // Outputs a Single-Page Application (SPA)
   distDir: 'build', // Changes the build output directory to `build`
 }
  
